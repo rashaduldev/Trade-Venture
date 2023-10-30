58b013452b4501ba6b7668899e54980fdb8c7956
noyon-podder <noyon.vai369@gmail.com>
2023-10-30 11:46:41 +0600

    navbar add this commit

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import logo from "../../assets/home/logo.png";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white py-[26px]">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div>
+            <Link href="/">
+              <Image src={logo} alt="Logo" width={121} />
+            </Link>
+          </div>
+
+          <nav>
+            <ul className="flex items-center gap-[30px]">
+              <li>
+                <Link
+                  href="/"
+                  className="text-lg font-medium text-main duration-300"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-lg font-normal text-text duration-300 hover:text-main hover:font-medium"
+                >
+                  Find Partner
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-lg font-normal text-text duration-300 hover:text-main hover:font-medium"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-lg font-normal text-text duration-300 hover:text-main hover:font-medium"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-lg font-normal text-text duration-300 hover:text-main hover:font-medium"
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="flex gap-4">
+            <button className="outline-none py-2 px-[25px] rounded-[8px] text-lg font-normal text-text bg-transparent border border-[#e4e4e4] duration-300 hover:bg-main hover:text-white hover:border-main">
+              Sign In
+            </button>
+            <button className="outline-none py-2 px-[25px] rounded-[8px] text-lg font-normal text-white bg-main border border-main duration-300 hover:bg-transparent hover:text-text hover:border-[#e4e4e4]">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "3rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        main: "#1C1C1C",
+        primary: "#17C550",
+        secondary: "#032D2C",
+        text: "#494949",
+        white: "#ffffff",
+        subText: "#777",
       },
     },
   },
   plugins: [],
-}
+};
