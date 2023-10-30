d1437b6610af2e515697c206f80c3586bc26eda2
noyon-podder <noyon.vai369@gmail.com>
2023-10-30 13:39:32 +0600

    about use section is completed

---
+++ src/components/home/About.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import about from "../../assets/home/about.png";
+const About = () => {
+  return (
+    <div className="py-[40px] md:py-[50px] lg:py-[95px] xl:py-[110px] container">
+      <div className="flex flex-col lg:flex-row items-center w-full lg:gap-[70px] gap-[30px]">
+        <div className="xl:w-[588px] lg:w-[500px]">
+          <p className="text-[22px] font-normal text-text">About Us</p>
+          <h1 className="xl:text-[35px] lg:text-[28px] font-bold text-main mt-4 mb-[18px]">
+            Introducation To Best Investor Sratrup Company
+          </h1>
+          <p className="text-subText text-xs font-normal mb-[28px]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+          <button className="outline-none py-2 px-[25px] rounded-[8px] text-lg font-bold text-main bg-primary border border-primary duration-300 hover:bg-transparent hover:text-primary hover:border-primary">
+            Read More
+          </button>
+        </div>
+
+        <div className="">
+          <Image src={about} alt="" width={482} className="object-cover " />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -12,30 +12,30 @@ const Banner = () => {
         height: "auto",
       }}
     >
-      <div className="container pt-[70px] pb-[30px]">
+      <div className="container lg:pt-[70px] pt-[50px] pb-[20px] lg:pb-[30px]">
         <div className="grid grid-cols-1 lg:grid-cols-9">
           <div className="lg:col-span-6">
-            <h1 className="xl:text-[50px] lg:text-[38px] text-main font-bold">
+            <h1 className="xl:text-[50px] lg:text-[38px] text-[30px] text-main font-bold">
               Join Us as an Investor, Co-founder, and Pioneer in the Startup
               Revolution!
             </h1>
 
-            <p className="xl:text-xl lg:text-lg font-normal text-main mt-[24px] mb-10">
+            <p className="text-base xl:text-xl lg:text-lg font-normal text-main mt-[24px] mb-10">
               Fueling Innovation and Success Through Strategic Collaboration and
               Investment Adventures
             </p>
 
-            <button className="outline-none py-2 px-[25px] rounded-[8px] text-lg font-normal text-white bg-main border border-main duration-300 hover:bg-transparent hover:text-text hover:border-[#e4e4e4]">
+            <button className="outline-none py-2 lg:px-[25px] px-[18px] rounded-[8px] text-lg font-normal text-white bg-main border border-main duration-300 hover:bg-transparent hover:text-text hover:border-[#e4e4e4]">
               Get Started
             </button>
           </div>
 
           <div className="lg:col-span-3 ">
-            <div className="flex items-end">
+            <div className="flex justify-center">
               <img
                 src="https://i.ibb.co/ww1BvhT/banner.png"
                 alt=""
-                className="xl:w-[578px] xl:h-[396px] lg:w-[448px] lg:h-[350px] object-cover"
+                className="w-[400px]  h-[320px] xl:w-[578px] xl:h-[396px] lg:w-[448px] lg:h-[350px] object-cover"
               />
             </div>
           </div>
