443a6285481851ac17d1c538304825b11fcbc70c
noyon-podder <noyon.vai369@gmail.com>
2023-11-08 11:37:00 +0600

    about page is changed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,11 @@
 import SectionTitle from "@/components/shared/sectionTitle/SectionTitle";
 import styles from "./AboutPage.module.css";
 
-import OurMission from "@/components/about/ourMission/ourMission";
 import WhyChoose from "@/components/about/WhyChoose/WhyChoose";
 import JoinForFutureTeam from "@/components/about/JoinForFutureTeam/JoinForFutureTeam";
 import TradeVentureTagLine from "@/components/about/TradeventureTagLine/TradeVentureTagLine";
 import AboutTradeVenture from "@/components/about/aboutTradeVenture/AboutTradeVenture";
+import OurMission from "@/components/about/ourMission/OurMission";
 
 const AboutPage = () => {
   return (
