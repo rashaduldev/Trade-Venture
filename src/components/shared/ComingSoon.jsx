907040c46fc15b8aa7bf0f6d31251ea765bc9f76
Noyon-Podder <noyon.vai369@gmail.com>
2023-12-17 14:10:24 +0600

    something is changed

--- src/components/shared/ComingSoon.jsx
+++ src/components/shared/ComingSoon.jsx
@@ -8,9 +8,6 @@ const ComingSoon = () => {
 
   return (
     <div className="h-screen w-full flex justify-center items-center flex-col">
-      {/* <h1 className="text-[68px] font-bold text-secondary capitalize ">
-        <span className="capitalize">{pageName}</span> <span>page</span>
-      </h1> */}
       <h2 className="lg:text-[96px] md:text-[48px] text-[28px] font-extrabold mt-10 text-main uppercase">
         {" "}
         C<span className="text-secondary">o</span>ming{" "}
