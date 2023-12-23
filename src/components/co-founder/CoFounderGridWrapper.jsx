1454ab959dbd118fd2c8a7513b7b7968eca7242a
Noyon-Podder <noyon.vai369@gmail.com>
2023-12-24 03:19:49 +0600

    some correction add in this commit

--- src/components/co-founder/CoFounderGridWrapper.jsx
+++ src/components/co-founder/CoFounderGridWrapper.jsx
@@ -3,7 +3,7 @@ import CoFounderProfileGrid from "./CoFounderProfileGrid";
 
 const CoFounderGridWrapper = () => {
   return (
-    <div className="container  lg:-mt-[150px] md:-mt-[100px] -mt-[75px]">
+    <div className="container  lg:-mt-[150px] md:-mt-[100px] -mt-[75px] mb-10 pb-5">
       <div className="z-50 relative bg-[#fff] border border-[#f1f1f1] rounded-md">
         <div className="grid grid-cols-1 md:grid-cols-9">
           <div className="md:col-span-3 lg:p-5">
